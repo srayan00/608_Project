18558a7c6aec421aed14f618b2dc109f47f247aa
srayan00 <srayan@umich.edu>
2024-04-18 22:08:22 -0400

    Ran jab again

--- test_sahana.py
+++ test_sahana.py
@@ -3,10 +3,15 @@ import ToyEnv
 import psrl
 from PosteriorSamplerGMM import GibbsSamplerGMM, HMCpymcGMM
 import time
+import json
+
+# Seed 1 = 42, Seed 2 = 43, Seed 3 = 44
 
 if __name__ == "__main__":
-    list_of_nS = [2, 3, 4]
-    sample_dict = sample_dict = {"Gibbs": {"state": {2: 0, 3: 0, 4: 0}}, "HMC": {"state": {2: 0, 3: 0, 4: 0}}}
+    seed = 42
+    np.random.seed(seed)
+    list_of_nS = [2, 4, 6, 8, 10]
+    sample_dict = sample_dict = {"Gibbs": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}, "HMC": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}}
     for nS in list_of_nS:
         new_env = ToyEnv.ToyEnv(nS, 3, 2)
         init_state = new_env.reset()
@@ -14,7 +19,7 @@ if __name__ == "__main__":
         alg_g = psrl.PSRL(env=new_env, sampler=GibbsSamplerGMM)
         _, _, policy_g = alg_g.run()
         end_time_g = time.time()
-        np.save(f"results/states/{nS}_policy_g.npy", policy_g)
+        np.save(f"results/states/{seed}_{nS}_policy_g.npy", policy_g)
         sample_dict["Gibbs"]["state"][nS] = end_time_g - st_time_g
 
         init_state = new_env.reset()
@@ -22,6 +27,7 @@ if __name__ == "__main__":
         alg_h = psrl.PSRL(env=new_env, sampler=HMCpymcGMM)
         _, _, policy_h = alg_h.run()
         end_time_h = time.time()
-        np.save(f"results/states/{nS}_policy_h.npy", policy_h)
+        np.save(f"results/states/{seed}_{nS}_policy_h.npy", policy_h)
         sample_dict["HMC"]["state"][nS] = end_time_h - st_time_h
-
+    with open(f"time_{seed}.json", "w") as f:
+        json.dump(sample_dict, f)
