6897aca98ae0f1e7b94b2e0baa32ac82f191cf7f
srayan00 <srayan@umich.edu>
2024-04-19 12:37:37 -0400

    More results with ncomps

--- test_ncomps.py
+++ test_ncomps.py
@@ -15,24 +15,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
     seed = args.seed
     np.random.seed(seed)
-    list_of_nS = [2, 4, 6, 8, 10]
-    sample_dict = sample_dict = {"Gibbs": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}, "HMC": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}}
-    for nS in list_of_nS:
-        new_env = ToyEnv.ToyEnv(nS, 3, 2)
+    list_of_ncomps = [2, 3, 4, 5]
+    sample_dict = sample_dict = {"Gibbs": {"ncomps": {2: 0, 3: 0, 4:0, 5:0}}, "HMC": {"ncomps": {2: 0, 3: 0, 4:0, 5:0}}}
+    for k in list_of_ncomps:
+        new_env = ToyEnv.ToyEnv(4, 3, k)
         init_state = new_env.reset()
         st_time_g = time.time()
         alg_g = psrl.PSRL(env=new_env, sampler=GibbsSamplerGMM)
         _, _, policy_g = alg_g.run()
         end_time_g = time.time()
-        np.save(f"results/states/{seed}_{nS}_policy_g.npy", policy_g)
-        sample_dict["Gibbs"]["state"][nS] = end_time_g - st_time_g
+        np.save(f"results/ncomps/{seed}_{k}_policy_g.npy", policy_g)
+        sample_dict["Gibbs"]["ncomps"][k] = end_time_g - st_time_g
 
         init_state = new_env.reset()
         st_time_h = time.time()
         alg_h = psrl.PSRL(env=new_env, sampler=HMCpymcGMM)
         _, _, policy_h = alg_h.run()
         end_time_h = time.time()
-        np.save(f"results/states/{seed}_{nS}_policy_h.npy", policy_h)
-        sample_dict["HMC"]["state"][nS] = end_time_h - st_time_h
-    with open(f"time_{seed}.json", "w") as f:
+        np.save(f"results/ncomps/{seed}_{k}_policy_h.npy", policy_h)
+        sample_dict["HMC"]["ncomps"][k] = end_time_h - st_time_h
+    with open(f"time_comps_{seed}.json", "w") as f:
         json.dump(sample_dict, f)
