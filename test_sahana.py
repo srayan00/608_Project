90ea7386bd171d2c49920a1d5d69f1b763189538
srayan00 <srayan@umich.edu>
2024-04-19 21:02:31 -0400

    changed the test script

--- test_sahana.py
+++ test_sahana.py
@@ -5,6 +5,7 @@ from PosteriorSamplerGMM import GibbsSamplerGMM, HMCpymcGMM
 import time
 import json
 import argparse
+import pickle
 
 parser = argparse.ArgumentParser(description='Run the PSRL algorithm on the ToyEnv environment')
 parser.add_argument('--seed', type=int, default=43, help='The seed for the random number generator')
@@ -14,25 +15,30 @@ parser.add_argument('--seed', type=int, default=43, help='The seed for the rando
 if __name__ == "__main__":
     args = parser.parse_args()
     seed = args.seed
-    np.random.seed(seed)
     list_of_nS = [2, 4, 6, 8, 10]
     sample_dict = sample_dict = {"Gibbs": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}, "HMC": {"state": {2: 0, 4: 0, 6:0, 8:0, 10:0}}}
     for nS in list_of_nS:
-        new_env = ToyEnv.ToyEnv(nS, 3, 2)
+        new_env = ToyEnv.ToyEnv(nS, 3, 2, seed)
+        f = open(f"results/states/env_{seed}_state{nS}.pkl", "wb")
+        pickle.dump(new_env.to_dict(), f)
+
         init_state = new_env.reset()
         st_time_g = time.time()
-        alg_g = psrl.PSRL(env=ToyEnv.ToyEnv(nS, 3, 2, np.random.default_rng(seed = seed)), sampler=GibbsSamplerGMM)
+        alg_g = psrl.PSRL(env=ToyEnv.ToyEnv(nS, 3, 2, seed), sampler=GibbsSamplerGMM)
         _, _, policy_g = alg_g.run()
         end_time_g = time.time()
-        np.save(f"results/states/{seed}_{nS}_policy_g.npy", policy_g)
+        g = open(f"results/states/{seed}_state{nS}_G.pkl", "wb")
+        pickle.dump(alg_g.to_dict(), g)
+        # np.save(f"results/states/{seed}_{nS}_policy_g.npy", policy_g)
         sample_dict["Gibbs"]["state"][nS] = end_time_g - st_time_g
 
         init_state = new_env.reset()
         st_time_h = time.time()
-        alg_h = psrl.PSRL(env=ToyEnv.ToyEnv(nS, 3, 2, np.random.default_rng(seed = seed)), sampler=HMCpymcGMM)
+        alg_h = psrl.PSRL(env=ToyEnv.ToyEnv(nS, 3, 2, seed), sampler=HMCpymcGMM)
         _, _, policy_h = alg_h.run()
         end_time_h = time.time()
-        np.save(f"results/states/{seed}_{nS}_policy_h.npy", policy_h)
+        h = open(f"results/states/{seed}_state{nS}_H.pkl", "wb")
+        pickle.dump(alg_h.to_dict(), h)
         sample_dict["HMC"]["state"][nS] = end_time_h - st_time_h
     with open(f"time_{seed}.json", "w") as f:
         json.dump(sample_dict, f)
