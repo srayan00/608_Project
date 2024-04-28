3d79d7726e68ca204b972cf32b6331981fba5384
srayan00 <srayan@umich.edu>
2024-04-28 17:44:03 -0400

    new results

---
+++ test_vi_state.py
@@ -0,0 +1,31 @@
+import numpy as np
+import ToyEnv
+import psrl
+from PosteriorSamplerGMM import GibbsSamplerGMM, HMCpymcGMM, VIpymcGMM
+import time
+import json
+import argparse
+import pickle
+
+parser = argparse.ArgumentParser(description='Run the PSRL algorithm on the ToyEnv environment')
+parser.add_argument('--seed', type=int, default=43, help='The seed for the random number generator')
+parser.add_argument("--state", type=int, default=100, help="State space")
+
+# Seed 1 = 42, Seed 2 = 43, Seed 3 = 44
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    seed = args.seed
+    nS = args.state
+    new_env = ToyEnv.ToyEnv(nS, 3, 2, seed)
+    with open(f"results/horizons/env_{seed}_horizon{h}.pkl", "wb") as f:
+        pickle.dump(new_env.to_dict(), f)
+
+    init_state = new_env.reset()
+    st_time_h = time.time()
+    print(f"Running HMC Sampler for {h}")
+    alg_h = psrl.PSRL(env=ToyEnv.ToyEnv(4, 3, 2, seed), sampler=VIpymcGMM, H = h, T = t)
+    _, _, policy_h = alg_h.run()
+    end_time_h = time.time()
+    with open(f"results/horizons/{seed}_horizon{h}_V.pkl", "wb") as h:
+        pickle.dump(alg_h.to_dict(), h)
